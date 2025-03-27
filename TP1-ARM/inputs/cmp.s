13d70021e454d0bd8fba512cb073df63278f51ac
tomasbenavidez <benavideztomas28@gmail.com>
2025-03-27 16:00:45 -0300

    comparador

--- TP1-ARM/inputs/cmp.s
+++ TP1-ARM/inputs/cmp.s
@@ -1,24 +1,24 @@
 .text
 
-MOV R0, #5      @ Load 5 into R0
-MOV R1, #10     @ Load 10 into R1
-CMP R0, R1      @ Compare R0 and R1
-BGT greater     @ Branch to 'greater' if R0 > R1
-BLT less        @ Branch to 'less' if R0 < R1
-BEQ equal       @ Branch to 'equal' if R0 == R1
+MOV R0, #5     
+MOV R1, #10    
+CMP R0, R1     
+BGT greater    
+BLT less       
+BEQ equal      
 
 greater:
-    MOV R2, #1      @ Set R2 to 1 (R0 > R1)
+    MOV R2, #1  
     B end
 
 less:
-    MOV R2, #2      @ Set R2 to 2 (R0 < R1)
+    MOV R2, #2  
     B end
 
 equal:
-    MOV R2, #0      @ Set R2 to 0 (R0 == R1)
+    MOV R2, #0  
     HLT 0
 
 end:
-    MOV R7, #1      @ Exit syscall
+    MOV R7, #1  
     HLT 0
