7d4c5814972f90d5fd102a9c33ce9ea4d477b9fa
Amato de Lusarreta, Franco <famatodelusarreta@udesa.edu.ar>
2025-03-28 11:12:14 -0300

    fixed?

--- TP1-ARM/inputs/mul.s
+++ TP1-ARM/inputs/mul.s
@@ -1,7 +1,13 @@
 .text
-adds x0, X0, 2       // Add 2 to x0
-adds x1, X1, 3       // Add 3 to x1
-mul x0, x0, 5       // Multiply x0 by 5
-mul x1, x1, 3       // Multiply x1 by 3
-mul x2, x0, x1      // Multiply x0 and x1, store result in x2
-HLT 0
+    adds x0, x0, 2        // Sumar 2 a x0
+    adds x1, x1, 3        // Sumar 3 a x1
+
+    MOV x3, 5             // Cargar 5 en x3
+    mul x0, x0, x3        // Multiplicar x0 por el valor en x3 (5)
+
+    MOV x3, 3             // Cargar 3 en x3
+    mul x1, x1, x3        // Multiplicar x1 por el valor en x3 (3)
+
+    mul x2, x0, x1        // Multiplicar x0 y x1, almacenar el resultado en x2
+    HLT 0
+    
